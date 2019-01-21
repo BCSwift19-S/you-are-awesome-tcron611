87273b65d52466f9c8424d12e0ab44076a3ae67f
Thomas Ronan <ronantho@bc.edu>
2019-01-20 23:52:20 -0500

    Week 11/ Week 2 Work Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -33,18 +33,6 @@ class ViewController: UIViewController {
         } else {
             index += 1
         }
-//        let message1 = "You Are Awesome!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        } else if messageLabel.text == message2 {
-//            messageLabel.text = message3
-//        } else {
-//            messageLabel.text = message1
-//
-//        }
    
     }
     
